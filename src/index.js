b078fec0df727121e5e56f8df317741231534b9b
kotapana <67315723+kotapana@users.noreply.github.com>
2021-05-12 22:04:51 +0900

    JS作成

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  addTodo(inputText);
+};
+
+// todoリストに追加
+const addTodo = (text) => {
   // li取得
   const li = document.createElement("li");
 
@@ -15,17 +20,48 @@ const onClickAdd = () => {
 
   // p取得,テキスト挿入
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   // 完了ボタン作成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    const completeTarget = deleteButton.parentNode.parentNode;
-    document.getElementById("incomplete-list").removeChild(completeTarget);
+    // 削除
+    const completeTarget = completeButton.parentNode.parentNode;
+    deleteFn(completeTarget);
 
-    document.getElementById("incomplete-list").removeChild(completeTarget);
-    alert("完了");
+    // 完了に追加
+    const completeText =
+      completeTarget.firstElementChild.firstElementChild.innerText;
+
+    completeTarget.textContent = null;
+
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // p取得,テキスト挿入
+    const p = document.createElement("p");
+    p.innerText = completeText;
+
+    // 戻すボタン作成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 削除
+      const deleteTarget = backButton.parentNode.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      const text = deleteTarget.firstElementChild.firstElementChild.innerText;
+      console.log(deleteTarget);
+      console.log(deleteTarget.firstElementChild);
+      console.log(deleteTarget.firstElementChild.firstElementChild);
+      addTodo(text);
+    });
+
+    // DOM生成して挿入
+    completeTarget.appendChild(div).appendChild(p);
+    completeTarget.appendChild(div).appendChild(backButton);
+    document.getElementById("complete-list").appendChild(completeTarget);
   });
 
   // 削除ボタン作成
@@ -33,7 +69,7 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     const deleteTarget = deleteButton.parentNode.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFn(deleteTarget);
   });
 
   // DOM生成して挿入
@@ -41,6 +77,11 @@ const onClickAdd = () => {
   li.appendChild(div).appendChild(completeButton);
   li.appendChild(div).appendChild(deleteButton);
   document.getElementById("incomplete-list").appendChild(li);
+};
+
+// 削除関数
+const deleteFn = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 // クリックイベント走らせる
